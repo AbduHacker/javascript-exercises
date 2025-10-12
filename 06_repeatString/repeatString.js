dcaa4296bc70dad38bebaaf07753d990128c8c90
AbduHacker <199925619+AbduHacker@users.noreply.github.com>
2025-10-12 17:31:27 +0530

    Solve the repeatString problem using loops

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
-
+function repeatString(str, count) {
+	let repeat = "";
+	if (count < 0)
+		return "ERROR"
+	for (let i = 0; i < count; i++) {
+		repeat += str;
+	}
+	return repeat;
+}
 // Do not edit below this line
 module.exports = repeatString;
